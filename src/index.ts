bd4a01c4864e6e23dcf011cb993ebd66437dddcd
Joseph <joseph300905@gmail.com>
2021-02-11 23:11:09 +0100

    Export App

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Session from './structures/session';
 import User from './structures/user';
 import Message from './structures/message';
+import App from './structures/app';
 
 export {
     Session,
     User,
     Message,
+    App,
 };
