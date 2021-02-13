aedf37e90fc6087f34ccfefeee76f10e8edd38b6
Joseph <joseph300905@gmail.com>
2021-02-13 10:53:49 +0100

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import Session from './structures/session';
-import User from './structures/user';
-import Message from './structures/message';
-import App from './structures/app';
-
-export {
-    Session,
-    User,
-    Message,
-    App,
-};
+export { default as Session, IAuthorizedAction, IChildren, IQuery, IUserInfo, IWidget, Type } from './structures/session';
+export { default as Message, IMessage, IMessageConfig } from './structures/message';
+export { default as User, UserPreview, IHobby, IUserPreview, IUser, profile } from './structures/user';
+export { default as App, IApp, scope, target } from './structures/app';
