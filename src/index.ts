45999c9c6fcbd15dde5d0ac70cc329ccf9d7149c
JanotLeLapin <joseph300905@gmail.com>
2021-04-25 22:47:41 +0200

    Format

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-export { default as Session, IAuthorizedAction, IChildren, IQuery, IUserInfo, IWidget, Type } from './structures/session';
-export { default as Message, IMessage, IMessageConfig } from './structures/message';
-export { default as User, UserPreview, IHobby, IUserPreview, IUser, profile } from './structures/user';
+export {
+  default as Session,
+  IAuthorizedAction,
+  IChildren,
+  IQuery,
+  IUserInfo,
+  IWidget,
+  Type,
+} from './structures/session';
+export {
+  default as Message,
+  IMessage,
+  IMessageConfig,
+} from './structures/message';
+export {
+  default as User,
+  UserPreview,
+  IHobby,
+  IUserPreview,
+  IUser,
+  profile,
+} from './structures/user';
 export { default as App, IApp, scope, target } from './structures/app';
